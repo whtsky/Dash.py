e9482a24d6bdd42123ca46b0c8c09187ff3dea4f
whtsky <whtsky@gmail.com>
2014-01-09 00:13:34 +0800

    Drop Python3 support

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Environment :: Console',
